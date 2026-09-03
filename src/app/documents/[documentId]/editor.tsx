0be24068cdd8fe10f7498658bf7abd8c8ef84a05
Ashish-Goyals <asbarnwal83036@gmail.com>
2026-09-03 23:24:03 +0530

    push

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,22 +1,24 @@
-"use client";
+'use client';
 
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import TaskItem from "@tiptap/extension-task-item";
-import TaskList from "@tiptap/extension-task-list";
-import Table from "@tiptap/extension-table";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
-import TableRow from "@tiptap/extension-table-row";
-import Image from "@tiptap/extension-image";
-import ImageResize from "tiptap-extension-resize-image";
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import Image from '@tiptap/extension-image';
+import ImageResize from 'tiptap-extension-resize-image';
+import { useEditorStore } from '@/store/use-editor-store';
 const Editor = () => {
+  const { setEditor } = useEditorStore();
   const editor = useEditor({
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right:56px;",
+        style: 'padding-left: 56px; padding-right:56px;',
         class:
-          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+          'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
     extensions: [
@@ -32,6 +34,9 @@ const Editor = () => {
       Image,
       ImageResize,
     ],
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
     content: `
         <table>
           <tbody>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import Link from 'next/link';
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center ">
-      Click{" "}
+      Click{' '}
       <Link href="/documents/123s" className="text-blue-500 hover:underline">
-        {" "}
+        {' '}
         &nbsp; &nbsp;
         <span className="text-blue-500 underline">here</span>
       </Link>
