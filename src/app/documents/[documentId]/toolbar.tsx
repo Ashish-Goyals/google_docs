845ce667765c7899721e953d4c7ffe6498fa7919
Ashish-Goyals <asbarnwal83036@gmail.com>
2026-09-05 20:20:16 +0530

    adding toolbar to the project with it's functionalities

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,20 @@
 'use client';
 
-import { LucideIcon, Undo2Icon } from 'lucide-react';
+import {
+  LucideIcon,
+  Undo2Icon,
+  Redo2Icon,
+  PrinterIcon,
+  SpellCheckIcon,
+  BoldIcon,
+  ItalicIcon,
+  UnderlineIcon,
+  MessageSquarePlusIcon,
+  ListTodoIcon,
+  RemoveFormattingIcon,
+} from 'lucide-react';
 import { useEditorStore } from '@/store/use-editor-store';
+import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -41,11 +54,97 @@ const Toolbar = () => {
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: 'Redo',
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: 'Print',
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: 'Spell Check',
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute('spellcheck');
+          editor?.view.dom.setAttribute(
+            'spellcheck',
+            current === 'false' ? 'true' : 'false'
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: 'Bold',
+        icon: BoldIcon,
+        isActive: editor?.isActive('bold'),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: 'Italic',
+        icon: ItalicIcon,
+        isActive: editor?.isActive('italic'),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      ,
+      {
+        label: 'Underline',
+        icon: UnderlineIcon,
+        isActive: editor?.isActive('underline'),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: 'Comment',
+        icon: MessageSquarePlusIcon,
+        onClick: () => console.log('TODO Comment'),
+        isActive: false, // TODO : Enable this functionality
+      },
+      {
+        label: 'List Todo',
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive('taskList'),
+      },
+      {
+        label: 'Remove Formatting',
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+      },
     ],
   ];
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* Todo font family   */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+
+      {/* Todo font heading   */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+
+      {/* Todo font szie   */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+
+      {/* TODO : Text Color  */}
+      {/* TODO : Highlight Color  */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/* TODO LINK  */}
+      {/* TODO Image  */}
+      {/* TODO Align  */}
+      {/* TODO Line Height  */}
+      {/* TODO List  */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
